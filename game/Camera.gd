51dbd0f5b44e5e4477c0fafc7a5ff07286af3e9a
eduardogmisiuk <eduardogmisiuk@gmail.com>
2018-07-10 22:02:48 -0300

    Made the camera movement.

--- game/Camera.gd
+++ game/Camera.gd
@@ -1,13 +1,14 @@
 extends Camera2D
 
-var delta_zoom
-
 # Constants
 const zoom_in_1 = Vector2(1.0, 1.0)
-const zoom_out_1 = Vector2(3.0, 3.0)
+const zoom_out_1 = Vector2(2.75, 2.75)
+const delta_zoom = Vector2(0.25, 0.25)
 
 func _init():
-	delta_zoom = Vector2(0.25, 0.25)
+	limit_bottom = Utils.map_limit.y
+	limit_right = Utils.map_limit.x
+	position = Vector2(limit_right, limit_bottom)/2 # Beginning at the centre of the map
 
 func _input(event):
 	# Right Mouse Button
@@ -25,4 +26,3 @@ func _input(event):
 	if Input.is_action_pressed("wheel_down") && zoom < zoom_out_1:
 		$Tween.interpolate_property(self, "zoom", zoom, zoom + delta_zoom,
 		0.25, Tween.TRANS_BACK, Tween.EASE_OUT)
-
